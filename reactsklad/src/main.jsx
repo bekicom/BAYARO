7bacee6ec475c77ec0c0d403999f3e519b30de14
bekicom <adelkauz6@gmail.com>
2026-05-01 11:53:12 +0500

    Fix desktop app routing

--- reactsklad/src/main.jsx
+++ reactsklad/src/main.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { store } from "./context/store";
 import { AuthProvider } from "./context/AuthContext";
 import { AppRouter } from "./router";
 import "./styles.css";
 
+const Router = window.location.protocol === "file:" ? HashRouter : BrowserRouter;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <Router>
         <AuthProvider>
           <AppRouter />
         </AuthProvider>
-      </BrowserRouter>
+      </Router>
     </Provider>
   </React.StrictMode>,
 );
